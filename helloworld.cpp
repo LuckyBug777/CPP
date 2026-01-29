57873285d2e06c4f5499adc8ef49847cf0200608
LuckyBug777 <cyberghostbug777@gmail.com>
2026-01-29 12:35:55 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+    //this is a comment 
+    /*
+    this is a multiline comment
+    */
+    std::cout << "Hello World" << std::endl;
+    std::cout << "How are you" << '\n';
+    return 0;
+}
