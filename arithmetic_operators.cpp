3384b79cb11aceb2a0bd2526158c12165d8ba3ee
LuckyBug777 <cyberghostbug777@gmail.com>
2026-02-01 15:50:13 +0530

    Update README and add new C++ programs for arithmetic operators, type conversion, and user input operations

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int students = 20;
+    students += 1;
+    cout<<students;
+    /*
+    add +
+    sub -
+    mul *
+    div /
+    rem %
+    precedence parentsis > mul & div > add & sub
+    */
+    return 0;
+}
--- typedef.cpp
+++ typedef.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pairList_t;
+typedef std::string text_t;
+
+
+using num_n = int;
+//using is more popular then typedef 
 
 int main(){
     
+    pairList_t pairlist;
+    text_t name = "king";
+    std::cout << name;
+    //helps with code readability and reduces  typos
     return 0;
 }
