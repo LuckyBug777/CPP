f117c102f7535033b79745a88e63161ca54609f3
LuckyBug777 <cyberghostbug777@gmail.com>
2026-04-09 11:13:24 +0530

    Add for loop program and update README

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    /*
+    for loops runs the block of code defined number of times.
+    */
+    for (int i = 10; i >= 1; i--)
+    {
+        std::cout << i << "\n";
+    }
+    std::cout << "Happy New Year\n";
+    return 0;
+}
